6f358e62182170b5a8c34c7f84d6679bbfb13aac
suhanyujie <suhanyujie@qq.com>
2022-01-30 22:49:33 +0800

    add some comment

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,9 @@
-
+/// crate 级别的错误类型
 pub enum ClientError {
 
 }
 
 // pub type Result<T> = std::result::Result<T, ClientError>;
+
+/// 基于 anyhow::Error 错误类型的 Result 类型别名
 pub type Result<T> = std::result::Result<T, anyhow::Error>;
